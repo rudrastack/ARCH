e950ed1f188ea030c3c06ef934e74757749c8690
Rudra Choudhary <rudrachoudhary067@gmail.com>
2026-08-23 16:57:29 +0530

    fix vercel api rewrites

--- Backend/src/controllers/auth.controller.js
+++ Backend/src/controllers/auth.controller.js
@@ -107,7 +107,7 @@ export const googleCallback = async (req, res) => {
     res.cookie("token", token, {
         httpOnly: true,
         secure: true,
-        sameSite: "lax",
+        sameSite: "none",
         maxAge: 7 * 24 * 60 * 60 * 1000,
     });
 
