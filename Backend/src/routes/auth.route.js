c6193a2d208ea3063ad001a4d315b5ab9ef63bf0
Rudra Choudhary <rudrachoudhary067@gmail.com>
2026-08-23 16:35:22 +0530

    fix: update google callback failureRedirect to use config.FRONTEND_URL

--- Backend/src/routes/auth.route.js
+++ Backend/src/routes/auth.route.js
@@ -16,7 +16,7 @@ router.post("/login", validateLoginUser, login)
 
 //@route /api/auth/google
 router.get("/google", passport.authenticate("google", { scope: ["profile", "email"] }))
-router.get("/google/callback", passport.authenticate("google", { session: false, failureRedirect: "http://localhost:5173/login" }),
+router.get("/google/callback", passport.authenticate("google", { session: false, failureRedirect: `${config.FRONTEND_URL}/login` }),
     googleCallback,
 )
 
